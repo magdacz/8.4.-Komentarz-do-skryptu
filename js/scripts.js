6c29cf3021f31d808a79e9d0375eef6fd8e04110
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2018-04-01 21:15:02 +0200

    Add index.htm and comments in js

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+var first/*Sentence*/ = 'I like a song that is called "Only you".';
+/*var*/ secondSentence = "I love a song that is called 'Only you'.";
